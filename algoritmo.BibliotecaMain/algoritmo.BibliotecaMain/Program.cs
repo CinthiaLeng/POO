ccd93be379a589cbb725eb284733bffbaed6b433
Cinthia Lenguer <116901617+CinthiaLeng@users.noreply.github.com>
2025-09-24 23:41:48 -0300

    Add files via upload

---
+++ algoritmo.BibliotecaMain/algoritmo.BibliotecaMain/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("***Bienvenido a la Biblioteca del Equipo 3***\nPor favor, elija una opción: ");
+
+
+Console.WriteLine("1 = Agregar lector\n2 = Agregar libro\n3 = Listar libros\n4 = Prestar libros\n5 = Devolver libros\n6 = Salir");
+
+
+string option = Console.ReadLine();
+
+
+do
+{
+
+    switch(option)
+    {
+        case "1":
+
+            break;
+
+        case "2":
+
+            break;
+
+        case "3":
+
+            break;
+
+        case "4":
+
+            break;
+
+        case "5":
+
+            break;
+
+        case "6":
+            Console.WriteLine("Saliendo del sistema... \nQue tenga un lindo día!" );
+            break;
+
+        default:
+            Console.WriteLine("Ingrese una opción válida: \n1 = Agregar lector\n2 = Agregar libro\n3 = Listar libros\n4 = Prestar libros\n5 = Devolver libros\n6 = Salir");
+            
+            option = Console.ReadLine();
+
+            Console.WriteLine("Gracias!");
+            break;
+
+    }
+
+
+
+
+
+} while(option != "6");
+
+
+
+
+
+
+Console.ReadKey();
